c4f72ac5b66a8da39a950989958f51e736f3a845
MarKie <116021405+TheCatstronautFromEarth@users.noreply.github.com>
2022-10-23 15:54:12 +0200

    Add files via upload
    Init

---
+++ src/HexToDec.java
@@ -0,0 +1,13 @@
+public class HexToDec {
+	
+	public static void outputDec(String StrHexCode) {
+		int valueRed, valueGreen, valueBlue;
+		valueRed = Integer.parseInt(StrHexCode.substring(0,2), 16);
+		valueGreen = Integer.parseInt(StrHexCode.substring(2,4), 16);
+		valueBlue = Integer.parseInt(StrHexCode.substring(4,6), 16);
+		WindowMixer.getSldRed().setValue(valueRed);
+		WindowMixer.getSldGreen().setValue(valueGreen);
+		WindowMixer.getSldBlue().setValue(valueBlue);
+		WindowMixer.setMasterValue((valueRed + valueGreen + valueBlue) / 3);
+	}
+}
---
+++ src/PanelOutput.java
@@ -0,0 +1,69 @@
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+
+public class PanelOutput {
+
+	private static JButton btnSetClipboard;
+	private static JTextField txtOutput;
+	private static Border loweredetched;
+	private static Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+	private static int xoffset = 12, yoffset = 18, buttonWidth = 90, textHeight = 24;
+	
+	static void panOut() {
+		
+		JPanel OutBox = new JPanel();
+		OutBox.setLayout(null);
+		OutBox.setBounds(15, 366, 230, 56);
+		OutBox.setOpaque(false);
+		loweredetched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		OutBox.setBorder(BorderFactory.createTitledBorder(loweredetched, "HEX Output"));
+		WindowMixer.getCp().add(OutBox);
+		
+		// Button Set value to clipboard 
+		btnSetClipboard = new JButton("Copy");				
+		btnSetClipboard.setBounds(xoffset, yoffset, buttonWidth, textHeight);
+		btnSetClipboard.setMnemonic('k');
+		btnSetClipboard.setToolTipText("Copy to clipboard");
+		btnSetClipboard.addActionListener(new ActionListener() {		
+			public void actionPerformed(ActionEvent event) {
+				handlebtnSetClipboard(event);
+			}
+		});
+		OutBox.add(btnSetClipboard);
+		
+		// TextField HEX output	
+		setTxtOutput(new JTextField());				
+		getTxtOutput().setBounds(110, yoffset, buttonWidth, textHeight);
+		WindowMixer.setTxtOut((WindowMixer.getValueRed() < 16 ? "0":"") + Integer.toHexString(WindowMixer.getValueRed()) + (WindowMixer.getValueGreen() < 16 ? "0":"") + Integer.toHexString(WindowMixer.getValueGreen()) + (WindowMixer.getValueBlue() < 16 ? "0":"") + Integer.toHexString(WindowMixer.getValueBlue()));
+    	getTxtOutput().setText(String.valueOf(WindowMixer.getTxtOut()));
+    	OutBox.add(getTxtOutput());
+    	
+	}
+	
+	// Set Hex value to clipboard
+	protected static void handlebtnSetClipboard(ActionEvent event) {
+		StringSelection stringSelection = new StringSelection(getTxtOutput().getText());
+		cb.setContents(stringSelection, null);
+	}
+
+	
+	// Setter Getter
+	
+	public static JTextField getTxtOutput() {
+		return txtOutput;
+	}
+
+	public static void setTxtOutput(JTextField txtOutput) {
+		PanelOutput.txtOutput = txtOutput;
+	}
+}
